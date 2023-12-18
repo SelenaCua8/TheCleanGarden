45855743cb0c83d710698d46cfadad33e6062453
Selena <Selenac99@hotmail.com>
2023-12-18 17:14:06 -0300

    agregando card content

--- Modelo1/js/index.js
+++ Modelo1/js/index.js
@@ -97,3 +97,21 @@ function checkPosition() {
 window.addEventListener('scroll', checkPosition);
 
 checkPosition();
+
+
+//CARD CONTENT
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
